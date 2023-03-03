5fb1e3154ee2048e8d055dbe3fd44d3750785bcf
Mohamed Amer <amermohamed0704@gmail.com>
2023-03-03 00:39:31 -0800

    update interface impl

--- src/api/Identity.ts
+++ src/api/Identity.ts
@@ -1,6 +1,10 @@
-import { IIdentity } from "../types";
+import { IdentityMeta, IdentityOptions, IIdentity } from "../types";
 
 class Identity implements IIdentity {
+    meta!: IdentityMeta;
+    createDid(options: IdentityOptions): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
 
 }
 
